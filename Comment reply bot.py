dbef25e1bf6a44f24c553801b49bac15048735cf
John Hadden <59323260+jhadden04@users.noreply.github.com>
2022-03-17 00:13:43 +0000

    Update Comment reply bot.py

--- Comment reply bot.py
+++ Comment reply bot.py
@@ -1,15 +1,15 @@
 import praw
 import time
-reddit = praw.Reddit(client_id='aCsxA497160GjRop_8DDXA',
-                     client_secret='rAuMc2mxJPwLMsX4xYqNrpg5ifa7ig',
+reddit = praw.Reddit(client_id='*******',
+                     client_secret='*****',
                      user_agent='a reddit instance',
-                     username='DistinctCap2574',
-                     password='this is the password',
+                     username='*****',
+                     password='*****',  # I will change this
                      check_for_async=False)
-trigger_phrases = ["why does this happen (1)", "why does that happen (2)"]
-comment_replies ["because of that (1)", "because of this (2)"]
-:
-for comment in reddit.subreddit("finance+askreddit").stream.comments():
+trigger_phrases = ["why does this happen (1)", "why does that happen (2)"]  # phrases to trigger the bot, change and add to this to suit
+comment_replies ["because of that (1)", "because of this (2)"]  # Phrases that respond to the trigger, 1 responds to 1 and 2 responds to 2
+# Use lower case and keep the formatting the same as shown (" and , in the same places)
+for comment in reddit.subreddit("finance+askreddit").stream.comments():  # change the subreddit names (finance+askreddit) for your desired subreddit: case-sensitive and needs the + sign between each new subreddit
  try:
   for i in range(len(trigger_phrases)):
     if trigger_phrases[i] in comment.body.lower():
